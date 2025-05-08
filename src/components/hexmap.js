8287e9aec1a949c2d5b5507399648c06f9fda444
Billy-Jack <bg2018.hw.ac.uk>
2025-05-08 12:29:39 +0100

    added hexgrid

---
+++ src/components/hexmap.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import { HexGrid, Layout, Hexagon } from 'react-hexgrid';
+
+const HexMap = () => {
+  const hexagons = [];
+
+  // Generate a simple hex grid
+  for (let q = -5; q <= 5; q++) {
+    for (let r = -5; r <= 5; r++) {
+      const s = -q - r;
+      if (Math.abs(s) <= 5) {
+        hexagons.push({ q, r, s });
+      }
+    }
+  }
+
+  return (
+    <div style={{ overflow: 'scroll', width: '100vw', height: '100vh' }}>
+      <HexGrid width={2000} height={2000}>
+        <Layout size={{ x: 10, y: 10 }} flat={false} spacing={1.1} origin={{ x: 0, y: 0 }}>
+          {hexagons.map(({ q, r, s }, i) => (
+            <Hexagon key={i} q={q} r={r} s={s} />
+          ))}
+        </Layout>
+      </HexGrid>
+    </div>
+  );
+};
+
+export default HexMap;
