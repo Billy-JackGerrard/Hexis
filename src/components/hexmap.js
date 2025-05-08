536cca261c22bce72bb1fd8f6459c2cf55b514d1
Billy-Jack <bg2018@hw.ac.uk>
2025-05-08 13:20:51 +0100

    made hexes smaller

--- src/components/hexmap.js
+++ src/components/hexmap.js
@@ -18,7 +18,7 @@ const HexMap = () => {
   return (
     <div style={{ overflow: 'scroll', width: '100vw', height: '100vh' }}>
       <HexGrid width={2000} height={2000}>
-        <Layout size={{ x: 10, y: 10 }} flat={false} spacing={1.1} origin={{ x: 0, y: 0 }}>
+        <Layout size={{ x: 5, y: 5 }} flat={false} spacing={1.1} origin={{ x: 0, y: 0 }}>
           {hexagons.map(({ q, r, s }, i) => (
             <Hexagon key={i} q={q} r={r} s={s} />
           ))}
