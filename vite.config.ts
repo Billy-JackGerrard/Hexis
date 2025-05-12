b42e0d471ba74a3c648a663be1a065da29fcb76a
Billy-Jack <bg2018@hw.ac.uk>
2025-05-12 13:16:37 +0100

    tidied up some files.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     open: true   // Automatically open browser
   },
   build: {
-    outDir: 'dist',
+    outDir: 'build',  // Output folder for build
     emptyOutDir: true
   }
 })
